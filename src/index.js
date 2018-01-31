94856660d5557940dcaa270bf0b6128b34d33214
luisf11 <luisfernandop1110@gmail.com>
2018-01-31 16:31:07 -0400

    First Commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import { injectGlobal } from 'styled-components';
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
+
+injectGlobal`
+  body {
+    background-color: ghostwhite;
+    padding: 0;
+    margin: 0;
+    font-family: cursive;
+  }
+`
